fa7f30a4bf7ba0b5737603f57355560b5cec6996
Cen Yee <redoctobers.1025@proton.me>
2026-09-20 21:15:51 +0800

    Added basic logout system

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useUserContext } from '../context/UserContext';
 
 export interface HeaderProps {
   setShowRegisterForm: (bool:boolean) => void;
@@ -7,12 +8,22 @@ export interface HeaderProps {
 
 export const Header : React.FC<HeaderProps> = ({setShowRegisterForm, setShowLoginForm}) => {
 
+    const { user, logout } = useUserContext();
+
     return (
         <header className="app-header">
           <h1>React + Node.js + MongoDB User App</h1>
           <nav className="nav">
-            <a onClick={() => {setShowRegisterForm(true)}}>Register</a>
-            <a onClick={() => {setShowLoginForm(true)}}>Login</a>
+            {user != null ? 
+              (<a onClick={() => {logout()}}>Log Out</a>)
+            : 
+              (
+                <>
+                <a onClick={() => { setShowRegisterForm(true); } }>Register</a>
+                <a onClick={() => { setShowLoginForm(true); } }>Login</a>
+                </>
+              )
+            }
           </nav>
         </header>
     )
